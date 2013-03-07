d4e91931b496a4d33852389213b400ddee3efb81
Connor Blanck <connorblanck@gmail.com>
2013-03-07 13:36:31 -0500

    Failure screen added with option to restart and death upon entry into the water

--- frogger/game.js
+++ frogger/game.js
@@ -6,15 +6,18 @@ function Coords(x, y){
 function GameState(){
     this.coords = new Coords(187, 503);
     this.lastDirection = "up";
+    this.deathTimer = -1;
     this.onLog = -1;
-    this.lives = 5;
-    this.gameover = false;
+    this.lives = 1;
     this.score = 0;
     this.highscore = 0;
     this.level = 1;
     this.time = 100;
-    this.vehicles = new Array(generateCar(1), generateCar(2), generateCar(3), generateCar(4), generateCar(5), generateCar(6));
+    this.vehicles = new Array(generateCar(1, 50), generateCar(2), generateCar(3), generateCar(4), generateCar(5), generateCar(6));
     this.logs = new Array(generateLog(1), generateLog(2), generateLog(3), generateLog(4), generateLog(5), generateLog(6));
+    this.hasLives = function (){
+        return (this.lives > 0);
+    };
 }
 
 function Log(x, y, type, row, dir, speed){
@@ -52,8 +55,8 @@ function Car(x, y, type, row, dir, speed){
     this.speed = speed;
     this.dir = dir;
     this.type = type;
-    this.width = this.type==1 ? 28 : this.type==2 ? 28 : this.type==3 ? 24 : 46;
-    this.height = this.type==1 ? 19 : this.type==2 ? 24 : this.type==3 ? 26 : 18;
+    this.width = this.type==1 ? 28 : this.type==2 ? 28 : this.type==3 ? 24 : this.type==4 ? 46 : 24;
+    this.height = this.type==1 ? 19 : this.type==2 ? 24 : this.type==3 ? 26 : this.type==4 ? 18 : 21;
     this.render = function(){
         switch(this.type){
             case 1:
@@ -67,6 +70,9 @@ function Car(x, y, type, row, dir, speed){
                 break;
             case 4:
                 ctx.drawImage(spriteSheet, 106, 302, 46, 18, this.coords.x, this.coords.y, 46, 18);
+                break;
+            case 5:
+                ctx.drawImage(spriteSheet, 11, 301, 24, 21, this.coords.x, this.coords.y, 24, 21);
                 break;
         }
     }
@@ -85,19 +91,27 @@ function start_game(){
     spriteSheet = new Image();
     spriteSheet.src = "assets/frogger_sprites.png";
     $(document).keydown(function(e) {
+        if (gameState.hasLives() && gameState.deathTimer == -1){
             if (e.keyCode == 38){ 
                 console.log("up");
                 goUp();
             } else if (e.keyCode == 40){
                 console.log("down");
                 goDown();
-            }  else if (e.keyCode == 37){
+            } else if (e.keyCode == 37){
                 console.log("left");
                 goLeft();
-            }  else if (e.keyCode == 39){
+            } else if (e.keyCode == 39){
                 console.log("right");
                 goRight();
+            } else if (e.keyCode == 82){
+                gameState = new GameState();
+                console.log("New game!");
             }
+        } else if (e.keyCode == 82){
+            gameState = new GameState();
+            console.log("New game!");
+        }
     });
     spriteSheet.onload = function(){
         drawBackground();
@@ -110,15 +124,30 @@ function start_game(){
 }
 
 function gameLoop(){
-    if(has_lives()){
+    if(gameState.hasLives()){
         drawBackground();
         drawFooter();
         drawLogs();
         drawCars();
         drawFrogger();
     }else{
+        drawBackground();
+        drawLogs();
+        drawCars();
         failureScreen();
+        drawFooter();
+        drawLives();
     }
+}
+
+function failureScreen(){
+    ctx.font = "bold 30pt arial";
+    ctx.fillStyle = "FF0000";
+    ctx.strokeStyle = "000000";
+    ctx.fillText("You Lose!", 100, 300);
+    ctx.strokeText("You Lose!", 100, 300);
+    ctx.fillText("Press r to restart", 50, 340);
+    ctx.strokeText("Press r to restart", 50, 340);
 }
 
 function goUp(){
@@ -145,29 +174,25 @@ function goRight(){
     console.log(gameState.coords);
 }
 
-function has_lives(){
-    return (gameState.lives > 0);
-}
-
-function generateCar(row){
+function generateCar(row, x){
     switch (row){
         case 1:
-            return new Car(20, 40, 4, 1, 1, 1)
+            return new Car( x==null ? 390 : x, 322, 4, 1, 1, 2)
             break;
         case 2:
-            return new Car(30, 50, 3, 2, 1, 1)
+            return new Car( x==null ? 390 : x, 350, 3, 2, 1, 2)
             break;
         case 3:
-            return new Car(40, 60, 2, 3, -1, 1)
+            return new Car( x==null ? -5 : x, 380, 2, 3, -1, 2)
             break;
         case 4:
-            return new Car(50, 70, 1, 4, 1, 1)
+            return new Car( x==null ? 390 : x, 412, 1, 4, 1, 2)
             break;
         case 5:
-            return new Car(60, 80, 2, 5, -1, 1)
+            return new Car( x==null ? -5 : x, 440, 5, 5, -1, 2)
             break;
         case 6:
-            return new Car(70, 90, 3, 6, 1, 1)
+            return new Car( x==null ? 390 : x, 468, 3, 6, 1, 2)
             break;
     }
 }
@@ -210,15 +235,33 @@ function drawCars(){
 }
 
 function drawFrogger(){
-    if(gameState.lastDirection == "up"){
-        ctx.drawImage(spriteSheet, 12, 369, 23, 17, gameState.coords.x, gameState.coords.y, 23, 17);
-    } else if (gameState.lastDirection == "down"){
-        ctx.drawImage(spriteSheet, 80, 369, 23, 17, gameState.coords.x, gameState.coords.y, 23, 17);
-    } else if (gameState.lastDirection == "left"){
-        ctx.drawImage(spriteSheet, 82, 335, 18, 23, gameState.coords.x, gameState.coords.y-3, 18, 23);
-    } else if (gameState.lastDirection == "right"){
-        ctx.drawImage(spriteSheet, 13, 334, 17, 23, gameState.coords.x, gameState.coords.y-3, 17, 23);
-    } 
+    if (gameState.deathTimer > 0) {
+        console.log("draw dead frogger?");
+        ctx.drawImage(spriteSheet, 251, 222, 18, 24, gameState.coords.x, gameState.coords.y, 18, 24);
+        gameState.deathTimer--;
+    } else if (gameState.deathTimer == 0){
+        gameState.coords.x = 187;
+        gameState.coords.y = 503;
+        gameState.deathTimer = -1;
+    } else if (waterCollide()){
+       console.log("waterCollided");
+       die();
+    } else { 
+        if(gameState.lastDirection == "up"){
+            ctx.drawImage(spriteSheet, 12, 369, 23, 17, gameState.coords.x, gameState.coords.y, 23, 17);
+        } else if (gameState.lastDirection == "down"){
+            ctx.drawImage(spriteSheet, 80, 369, 23, 17, gameState.coords.x, gameState.coords.y, 23, 17);
+        } else if (gameState.lastDirection == "left"){
+            ctx.drawImage(spriteSheet, 82, 335, 18, 23, gameState.coords.x, gameState.coords.y-3, 18, 23);
+        } else if (gameState.lastDirection == "right"){
+            ctx.drawImage(spriteSheet, 13, 334, 17, 23, gameState.coords.x, gameState.coords.y-3, 17, 23);
+        } 
+    }
+}
+
+function die(){
+    gameState.lives--;
+    gameState.deathTimer = 30;
 }
 
 function drawFooter(){
@@ -262,4 +305,16 @@ function drawBackground(){
     ctx.drawImage(spriteSheet, 0, 0, 399, 113, 0, 0, 399, 113);
     ctx.drawImage(spriteSheet, 0, 119, 399, 34, 0, 283, 399, 34);
     ctx.drawImage(spriteSheet, 0, 119, 399, 34, 0, 495, 399, 34);  
+}
+
+function collide (x1, y1, w1, h1, x2, y2, w2, h2){
+    
+}
+
+function waterCollide (){
+    if(gameState.onLog == -1 && gameState.coords.y > 105 && gameState.coords.y < 270){
+        return true;
+    } else {
+        return false;
+    }
 }
